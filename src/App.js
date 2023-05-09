2043320c5a3091223af8e86813b5637b1a37a53a
Peester <oliversladkovsky@gmail.com>
2023-05-10 00:12:29 +0200

    Javascript - funkcie MAP a FILTER

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-
+import data from './data'
 
 
 const App = () => {
+
+  // javascript function map
+
+    const mojePole = [1,5,10,]
+
+    const vyslednePoleMap = mojePole.map( (jednoPole) => {
+      return jednoPole + 10
+    } )
+
+    console.log(vyslednePoleMap[0]) // result = 11 
+    console.log(vyslednePoleMap[1]) // result = 15 
+    console.log(vyslednePoleMap[2]) // result = 20 
+
+    const noveData = data.map( (jednaUloha) => {
+      return jednaUloha.name
+    })
+
+    console.log(noveData)
+
+  // javascript end - map
+  
+  // javascript function filter
+
+
+  const vymazaneID = 2
+
+  const vyslednePoleFilter = data.filter( (jednoPole) => {
+    return jednoPole.id != vymazaneID
+  })
+  
+  console.log(vyslednePoleFilter) // vráti všetky polia okrem pola s ID $vymazaneID
+
+  // javascript end - filter 
+
+
   return (
   <div className="">
     
