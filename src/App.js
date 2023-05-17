bc234c807ef4138a50135b67aa6d061f50179656
Peester <oliversladkovsky@gmail.com>
2023-05-17 19:54:50 +0200

    useEffect - cleanUp function

--- src/App.js
+++ src/App.js
@@ -2,42 +2,28 @@ import { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const [number, setNumber] = useState(0)
-  const [title, setTitle] = useState(0)
+const [windowSize, setWindowSize] = useState(window.innerWidth)
 
-  const count = () => {
-    const result = number + 1
-    setNumber(result)
+
+const windowSizeFunc = () => {
+  setWindowSize(window.innerWidth)
+}
+
+
+useEffect( () => {
+  console.log("cleanUp function")
+  window.addEventListener('resize', windowSizeFunc)
+  return () => {
+    window.removeEventListener('resize', windowSizeFunc)
   }
+})
 
 
-  useEffect(() => {
-    const btn = document.querySelector('.tlacitko')
-    if(number > 0 ){
-      btn.textContent = `kliknuté ${number} krát`
-    }else{
-      btn.textContent = "klikni"
-    }
-    console.log(btn)
-    
-  },[number])
-  
-  const titleChange = () => {
-    const result = title + 1
-    document.title = ("titulok kliknutý " + result + " krát")
-    setTitle(result)
-  }
-  useEffect(() => {    
-
-    console.log("bolo kliknuté na TITLE")
-
-  },[title])
 
   return (
   <div className="">
-    <h2> {number} </h2>
-    <button className="tlacitko" onClick={count}>Klikni</button>
-    <button className="tlacitkoTitle" onClick={titleChange}>Zmeň titulok</button>
+    <h1> Šírka obrazovky </h1>
+    <p> {windowSize} </p>
   </div>
   )
 }
