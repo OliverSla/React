d3a771a84a44eee707932d1d5a9cbe83361787f9
Peester <oliversladkovsky@gmail.com>
2023-05-18 22:47:40 +0200

    short circuit evaltion

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ const url = 'http://api.open-notify.org/iss-now.json'
 
 const App = () => {
 
-  const [loading, setLoading] = useState(false)
-  const [latitude, setLatitude] = useState('default')
-  const [longitude, setLongitude] = useState('default')
+  // Short circuit evalution
 
-  useEffect(() => {
-      fetch(url)
-          .then((response) => response.json())
-          .then((data) => data['iss_position'])
-          .then((data) => {
-            const longitude = (data['longitude'])
-            const latitude = (data['latitude'])
-            setLatitude(latitude)
-            setLongitude(longitude)
-          }) 
-          setLoading(true)
-  }, [])
+  const value ="notEmpty"
+  const value2 = 0
+  const value3 = false
+  const value4 = true
+  const value5 = ""
 
-      {
-        if(loading === true){
-          return <div> <h1>Stránka načítana </h1> <h2> {latitude} </h2> </div>
-        }else{
-          return <div> <h1> Stránka sa načítava </h1> <h2> {longitude} </h2> </div>
-        }
-      }
+  console.log(value3 && value5 ) // False && False = value3
+  console.log(value && value4 ) // True && True = value4
+  console.log(value5 && value ) // False && True = value5
 
+  
+  console.log(value3 || value5 ) // False && False = value5
+  console.log(value5 || value ) // False && True = value
+  console.log(value || value4 ) // True && True = value
+
+
+
+
+  return(
+    <div>
+
+    </div>
+  )
 }
 
 
